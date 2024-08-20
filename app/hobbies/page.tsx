8a77adc2b5ba01a889fb5744125e021877aca99f
Chase Little <chaselittle@Chases-MacBook-Pro-2.local>
2024-08-20 12:55:14 -0700

    placeholder for hobbies

--- app/hobbies/page.tsx
+++ app/hobbies/page.tsx
@@ -1,9 +1,14 @@
+import SvgAsset from "@/components/svg-assets";
 
 
 export default function HobbiesPage() {
     return (
-        <div>
-            <h1>Hobbies Page</h1>
+        <div className="flex flex-col justify-center items-center w-full h-full space-y-60">
+            Hobbies Page 
+            <div>
+                Currently Under Construction
+            </div>
+            <SvgAsset kind="project" href="/" size={300}/>
         </div>
     )
 }
